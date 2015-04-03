428f9f23c997a6cffba706d02dce346dab74c56d
Nick Loadholtes <nick@ironboundsoftware.com>
2015-04-02 21:13:30 -0400

    Functions from chapter 2

--- little_lisper.scm
+++ little_lisper.scm
@@ -1,1 +1,18 @@
 ; Code from "The Little LISPer"
+
+;; Chapter 2
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) t)
+     ((atom? (car l) t)
+      (t nil)))))
+
+(define member?
+  (lambda (a lat)
+    (cond 
+     ((null? lat) nil)
+     (t (or 
+	 (eq? (car lat) a)
+	 (member? a (cdr lat)))))))
+
